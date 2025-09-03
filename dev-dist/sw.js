416aee69464ae45952a12b3db3ab7ec9d8ed345a
Riso19 <www.sain1919@gmail.com>
2025-09-03 14:22:17 +0530

    changed app name

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-86c9b217'], function (workbox) {
     [
       {
         url: 'index.html',
-        revision: '0.oe40tn4u42',
+        revision: '0.2l29te6ka8o',
       },
     ],
     {},
